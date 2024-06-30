6a8c420c08d027f3a83b4af1a4a97e7b272cfa17
Cloxdyy <cloudgaming5709@gmail.com>
2024-06-30 02:27:35 -0500

    - examples

--- src/interactions/buttons/deleteOutput.js
+++
@@ -1,7 +0,0 @@
-export const Button = {
-    name: "deleteOutput",
-    ownerOnly: true,
-    run: (interaction) => {
-        interaction.message.delete();
-    }
-}; // ButtonCommand of the deleteOutput button.
--- src/interactions/selectMenus/selectMenuExample.js
+++
@@ -1,8 +0,0 @@
-export const Menu = {
-    name: "SelectMenuExample",
-    run: (interaction) => {
-        interaction.reply({
-            content: "Here is your cookie! :cookie:"
-        });
-    }
-}; // Code for SelectMenuExample SelectMenu
